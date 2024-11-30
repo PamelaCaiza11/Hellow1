89fd7db07a9456dc6681853dbf12a5f172e6d809
PamelaCaiza11 <3a.caiza.pamela@gmail.com>
2024-11-30 18:45:32 -0500

    Add name

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ server = WEBrick::HTTPServer.new(Port: 8080)
 
 server.mount_proc '/' do |req, res|
   res['Content-Type'] = 'text/html'
-  res.body = '<html><body><h1>Hello World</h1></body></html>'
+  res.body = '<html><body><h1>Hello World I am Pame Caiza</h1></body></html>'
 end
 
 trap('INT') { server.shutdown }
